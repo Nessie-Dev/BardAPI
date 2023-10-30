04d9ba660299bc4185cc2620e1eefd3b2376f9c9
Nessie-Dev <63102193+Nessie-Dev@users.noreply.github.com>
2023-10-30 16:44:45 +0000

    update

--- index.js
+++ index.js
@@ -2,9 +2,40 @@ import express from 'express';
 import sqlite3 from 'sqlite3';
 import Bard from 'bard-ai';
 import fetch from 'node-fetch';
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+
+async function readCookies() {
+  try {
+    const cookiesData = fs.readFileSync('cookies.json', 'utf8');
+    return JSON.parse(cookiesData);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      fs.writeFileSync('cookies.json', JSON.stringify({}, null, 2), 'utf8');
+      return {};
+    } else {
+      console.error('Error reading cookies:', error);
+      return null;
+    }
+  }
+}
+
+async function updateCookies(cookiesObject) {
+  try {
+    const existingCookies = readCookies();
+    const updatedCookies = { ...existingCookies, ...cookiesObject };
+    fs.writeFileSync('cookies.json', JSON.stringify(updatedCookies, null, 2), 'utf8');
+    console.log('Cookies updated successfully');
+  } catch (error) {
+    console.error('Error updating cookies:', error);
+  }
+}
+
 
 function cleanContentAndExtractImages(output) {
   const imageRegex = /\[Image of .+?\]\(.*?\)/g;
@@ -12,11 +43,11 @@ function cleanContentAndExtractImages(output) {
   const content = output.content.replace(imageRegex, '');
 
   const images = output.images.filter(image => image.tag && image.url && image.info)
-                             .map(image => ({
-                               tag: image.tag,
-                               url: image.url,
-                               info: image.info
-                             }));
+    .map(image => ({
+      tag: image.tag,
+      url: image.url,
+      info: image.info
+    }));
 
   if (images.length > 0) {
     return {
@@ -28,15 +59,9 @@ function cleanContentAndExtractImages(output) {
       content
     };
   }
-}
+};
 
-// Initialize Bard instance
-const bard = new Bard({
-  "__Secure-1PSID": "Zgiv2N92706_yTb-dIIm5DwrnKyuyThHZz4EAhowqnXNnyHS-S6VJwXh63lWz97UPvTHGA.",
-  "__Secure-1PSIDTS": "sidts-CjEBSAxbGfe63ipTpC0Uj3daPb9gHSaMSG7zDhi--6EkwheHhJlhxDO9OD86d2AyDGszEAA"
-});
 
-// Initialize SQLite database
 const db = new sqlite3.Database('chats.db', err => {
   if (err) {
     console.error('Error opening database:', err.message);
@@ -45,18 +70,51 @@ const db = new sqlite3.Database('chats.db', err => {
   }
 });
 
-// Create a new table to store chat data
+
 db.run(`
   CREATE TABLE IF NOT EXISTS chats (
     uid TEXT PRIMARY KEY,
     chat_export TEXT
   )
 `);
+let bard;
 
-// Endpoint to interact with Bard using GET
+app.post('/update', async (req, res) => {
+  console.log(`Request recived for updating cookies.`)
+  const data = req.body;
+
+  try {
+    if (!data || !data["__Secure-1PSID"] || !data["__Secure-1PAPISID"] || !data["__Secure-1PSIDCC"] || !data["__Secure-1PSIDTS"]) {
+      
+      return res.status(400).json({ error: 'Bad Request', message: 'Required cookies are missing.' });
+    }
+
+    
+    await updateCookies({
+      "__Secure-1PSID": data["__Secure-1PSID"],
+      "__Secure-1PAPISID": data["__Secure-1PAPISID"],
+      "__Secure-1PSIDCC": data["__Secure-1PSIDCC"],
+      "__Secure-1PSIDTS": data["__Secure-1PSIDTS"],
+    });
+
+    console.log('Cookies updated successfully.');
+    res.status(200).json({ message: 'Cookies updated successfully.' });
+  } catch (error) {
+    
+    res.status(500).json({ error: 'Internal Server Error', message: 'An error occurred while updating cookies.' });
+  }
+});
+
 app.get('/chat', async (req, res) => {
+  if (bard == undefined) {
+  bard = new Bard(await readCookies());
+  };
   try {
     const { uid, prompt, img } = req.query;
+    if (!prompt) {
+      res.status(400).send('Missing prompt parameter');
+      return;
+    }
 
     let options = { format: Bard.JSON };
 
@@ -66,13 +124,13 @@ app.get('/chat', async (req, res) => {
     }
 
     if (!uid) {
-      // Create a new chat
+
       const myConversation = bard.createChat();
       const response = await myConversation.ask(prompt, options);
       const modifiedContent = cleanContentAndExtractImages(response);
       res.json({ data: modifiedContent });
     } else {
-      // Check if chat exists in the database for the given UID
+
       db.get('SELECT chat_export FROM chats WHERE uid = ?', [uid], async (err, row) => {
         if (err) {
           console.error('Database error:', err.message);
@@ -81,18 +139,29 @@ app.get('/chat', async (req, res) => {
         }
 
         if (row) {
-          // Use existing chat export object to continue the conversation
+
           const existingChat = bard.createChat(JSON.parse(row.chat_export));
           const response = await existingChat.ask(prompt, options);
+          console.log(response)
           const modifiedContent = cleanContentAndExtractImages(response);
-          res.json({ data: modifiedContent });
+
+
+          const updatedChatExport = JSON.stringify(response.ids);
+          db.run('UPDATE chats SET chat_export = ? WHERE uid = ?', [updatedChatExport, uid], err => {
+            if (err) {
+              console.error('Database error:', err.message);
+              res.status(500).json({ error: 'An internal server error occurred.' });
+              return;
+            }
+            res.json({ data: modifiedContent });
+          });
         } else {
-          // Create a new chat for a new user
+         
           const newChat = bard.createChat();
           const response = await newChat.ask(prompt, options);
-          const chatExport = JSON.stringify(await newChat.export());
+          const chatExport = JSON.stringify(newChat.export());
 
-          // Store chat export object in the database
+          
           db.run('INSERT INTO chats (uid, chat_export) VALUES (?, ?)', [uid, chatExport], err => {
             if (err) {
               console.error('Database error:', err.message);
@@ -110,6 +179,7 @@ app.get('/chat', async (req, res) => {
     res.status(500).json({ error: 'An internal server error occurred.' });
   }
 });
+
 
 // Start the Express server
 app.listen(port, () => {
